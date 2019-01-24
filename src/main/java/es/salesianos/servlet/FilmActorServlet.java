4db6b290c7efdb69cbf1ef46187449c6db2aba5f
ignaciLou <igna.lm@gmail.com>
2019-01-24 09:33:10 +0100

    Signed-off-by: ignaciLou <igna.lm@gmail.com>

--- src/main/java/es/salesianos/servlet/FilmActorServlet.java
+++ src/main/java/es/salesianos/servlet/FilmActorServlet.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -38,7 +39,7 @@ public class FilmActorServlet {
 	}
 
 	protected void redirect(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/filmactors.jsp");
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/filmActor.jsp");
 		dispatcher.forward(req, resp);
 	}
 }
